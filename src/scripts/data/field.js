603df995c0df83e40dcee883deeb221a924ae4f1
maxim <maxim.shaforostov@gmail.com>
2019-12-25 23:55:13 +0200

    field as a table

--- src/scripts/data/field.js
+++ src/scripts/data/field.js
@@ -1,51 +1,31 @@
 const SMALL_SIDE = '50';
 const LARGE_SIDE = '100';
-const COLORS = ['white', 'pink', 'lightblue', 'magenta', 'orange', 'red', 'mint', 'green', 'blue'];
+const COLORS = ['white', '#b400ff', 'lightblue', 'magenta', 'orange', 'red', 'mint', 'green', 'blue'];
 
 const field = {
       cells: [
             {
                   index: 1,
-                  top: 650-SMALL_SIDE*2,
-                  left: 650-SMALL_SIDE*2,
-                  width: LARGE_SIDE,
-                  height: LARGE_SIDE,
                   colorClass: 'cell-color-top',
                   group: 0,
             },
             {
                   index: 2,
-                  top: 650-SMALL_SIDE*2,
-                  left: 650-SMALL_SIDE*3,
-                  width: SMALL_SIDE,
-                  height: LARGE_SIDE,
                   colorClass: 'cell-color-top',
                   group: 1,
             },
             {
                   index: 3,
-                  top: 650-SMALL_SIDE*2,
-                  left: 650-SMALL_SIDE*4,
-                  width: SMALL_SIDE,
-                  height: LARGE_SIDE,
                   colorClass: 'cell-color-top',
                   group: 1,
             },
             {
                   index: 4,
-                  top: 650-SMALL_SIDE*2,
-                  left: 650-SMALL_SIDE*5,
-                  width: SMALL_SIDE,
-                  height: LARGE_SIDE,
                   colorClass: 'cell-color-top',
                   group: 0,
             },
             {
                   index: 5,
-                  top: 650-SMALL_SIDE*2,
-                  left: 650-SMALL_SIDE*6,
-                  width: SMALL_SIDE,
-                  height: LARGE_SIDE,
                   colorClass: 'cell-color-top',
                   group: 2,
             },
@@ -55,10 +35,6 @@ const field = {
 
             {
                   index: 40,
-                  top: 650-SMALL_SIDE*3,
-                  left: 650-SMALL_SIDE*2,
-                  width: LARGE_SIDE,
-                  height: SMALL_SIDE,
                   colorClass: 'cell-color-left',
                   group: 8,
             }
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,6 +4,23 @@ import $ from 'jquery';
 
 $('body').append($('<div>').addClass('field'));
 
+const cellsArray = [];
+$('.table-field td').each((index, cell) => {
+    const id = $(cell).data('id');
+    cellsArray[id-1] = cell;
+});
+
+cellsArray.forEach((cell, num) => {
+    const fieldConfig = field.cells.find(item => item.index === num+1);
+    if (!fieldConfig) return;
+    const cellDiv = $('<div>').addClass('cell-content');
+    const cellColor = $('<div>').addClass(fieldConfig.colorClass);
+    cellColor.css('backgroundColor', fieldConfig.color);
+    cellDiv.append(cellColor);
+
+    $(cell).append(cellDiv);
+});
+
 field.cells.forEach((cell) => {
     $('.field').append(
         $('<div>').css({
